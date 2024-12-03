b037bd8a2d5137918fff6ed0e3158f6893b9541d
Pablo Santos <pablo.santos@suporterenova.onmicrosoft.com>
2024-12-03 17:00:42 -0300

    style: ajusta ordem das caixas

--- src/app/modules/main/gallery/gallery.component.ts
+++ src/app/modules/main/gallery/gallery.component.ts
@@ -183,7 +183,7 @@ export class GalleryComponent {
                   return item !== null;
                 })
                 .sort((a, b) => {
-                  return (a.secao ?? 0) - (b.secao ?? 0);
+                  return (b.secao ?? 0) - (a.secao ?? 0);
                 })
             : [];
         } else if (categoriaId === 2) {
@@ -202,7 +202,7 @@ export class GalleryComponent {
                   return item !== null;
                 })
                 .sort((a, b) => {
-                  return (a.secao ?? 0) - (b.secao ?? 0);
+                  return (b.secao ?? 0) - (a.secao ?? 0);
                 })
             : [];
         } else {
