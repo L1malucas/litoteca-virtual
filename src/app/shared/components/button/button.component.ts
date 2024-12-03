888a115f558d9e2197579837a35eabb177dff905
Pablo Santos <pablo.santos@suporterenova.onmicrosoft.com>
2024-12-03 13:25:39 -0300

    style: ajustes de estilo

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from "@angular/core";
 })
 export class ButtonComponent {
   @Input() text: string = "";
+  @Input() icon: string = "";
   @Input() type: string = "button";
   @Input() disabled: boolean = false;
   @Input() onClick: any = () => {};
