e7b69a4fad94659c08593099214cc0799d742083
Pablo Santos <pablo.santos@suporterenova.onmicrosoft.com>
2024-12-12 13:56:24 -0300

    feat: salva dados do usuário em cache

--- src/app/shared/components/header-home/header-home.component.ts
+++ src/app/shared/components/header-home/header-home.component.ts
@@ -1,8 +1,4 @@
 import { Component, Input } from "@angular/core";
-import { HelpConfig } from "@config/help-config";
-import { Token } from "@models/system/token.model";
-import { UserService } from "@services/system/user.service";
-import { jwtDecode } from "jwt-decode";
 
 @Component({
   selector: "app-header-home",
@@ -12,58 +8,31 @@ import { jwtDecode } from "jwt-decode";
 export class HeaderHomeComponent {
   @Input() styleHeader: { [klass: string]: any } = {};
   show: boolean = false;
+  user!: any;
 
-  token: Token | null = null;
-  emailUser!: string;
-
-  user = {
-    name: "",
-    info: "",
-    email: "",
-    image: "assets/images/jpeg/image_placeholder.jpg",
-    id: "",
-  };
-
-  constructor(
-    private _userService: UserService,
-    private _helpConfig: HelpConfig,
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.getUserInfoLogged();
-    this.getUserInfo(this.emailUser);
   }
 
-  getUserInfoLogged() {
-    const tokenString = localStorage.getItem("ngx-webstorage:STORAGE_AUTH");
-    if (tokenString) {
-      try {
-        this.token = JSON.parse(tokenString) as Token;
-        if (this.token?.access_token) {
-          const decodedToken: any = jwtDecode(this.token.access_token);
-          this.emailUser = decodedToken.email;
-        }
-      } catch (error) {
-        console.error("Erro ao analisar o token do localStorage:", error);
-        this.token = null;
+  getUserInfoLogged(): void {
+    try {
+      const user = localStorage.getItem("ngx-webstorage:USER_INFO");
+      if (user) {
+        const parsedUser = JSON.parse(user);
+        this.user = parsedUser;
+        console.log(parsedUser);
+        return parsedUser;
+      } else {
+        console.warn("No user information found in localStorage.");
       }
-    } else {
-      console.warn("Token não encontrado no localStorage.");
-      this.token = null;
+    } catch (error) {
+      console.error(
+        "Failed to parse user information from localStorage:",
+        error,
+      );
     }
-  }
-
-  getUserInfo(email: string) {
-    this._userService.getUserByEmail(email).subscribe((user: any) => {
-      this.user.id = user.data[0].id;
-      this.user.name = user.data[0].nome + " " + user.data[0].sobrenome;
-      this.user.info = user.data[0].profissao;
-      this.user.email = user.data[0].email;
-      this.user.image =
-        user.data[0].fotoReferenceFtp != ""
-          ? `${this._helpConfig.FTP_URL}${user.data[0].fotoReferenceFtp.replace(/\\/g, "/")}`
-          : "./assets/img/image_placeholder.jpg";
-    });
   }
 
   openProfile() {
